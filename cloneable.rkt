2fa2ba455b6777967cc5d98ad2796003b441ff11
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2024-01-19 19:58:30 -0600

    Add cloneable.

---
+++ cloneable.rkt
@@ -0,0 +1,55 @@
+#lang racket
+
+;;; File:
+;;;   cloneable.rkt
+;;; Summary:
+;;;   Tools for dealing with cloneable objects
+;;; Author:
+;;;   Samuel A. Rebelsky
+
+(require racket/generic)
+(require "sstruct.rkt")
+
+(provide (all-defined-out))
+
+; +---------------+--------------------------------------------------
+; | The interface |
+; +---------------+
+
+;;; (cloneable? val) -> boolean?
+;;;   val : any?
+;;; Determine if val is cloneable. You can call `clone` on cloneable values.
+(define-generics cloneable
+  (clone cloneable)
+  #:fallbacks
+  [(define clone
+     (lambda (val)
+       (error 'clone "~a is not cloneable" val)))])
+
+; +-----------------------+------------------------------------------
+; | A deprecated approach |
+; +-----------------------+
+
+;;; (struct-cloneable name ...) -> (void?)
+;;;   name : identifier?
+;;; Make a cloneable structure.
+;;;
+;;; Deprecated.
+(define-syntax struct-cloneable
+  (syntax-rules ()
+    [(struct-cloneable name rest ...)
+     (struct name rest ...
+       #:methods gen:cloneable
+       [(define clone
+          (lambda (val)
+            (struct-copy name val)))])]))
+
+; +---------------------+--------------------------------------------
+; | The #:cloneable tag |
+; +---------------------+
+
+(set-sstruct-tag! #:cloneable 0
+                  (lambda (name)
+                    `(#:methods gen:cloneable
+                      [(define clone
+                         (lambda (val) (struct-copy ,name val)))])))
